762782bbc606f361a0f71c485cce00fdcec847d6
Roy Eyal <roy.eyal@gmail.com>
2024-06-03 12:19:27 +0300

    Create resnfield.js

---
+++ resnfield.js
@@ -0,0 +1,20 @@
+ document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const boxes = gsap.utils.toArray('.solution');
+boxes.forEach((box, i) => {
+    const anim = gsap.fromTo(
+        box,
+        { autoAlpha: 0, y: 50 },
+        { duration: 1, autoAlpha: 1, y: 0, delay: 0.5 + (0.5 * i) }
+    );
+
+    ScrollTrigger.create({
+        trigger: box,
+        animation: anim,
+        toggleActions: 'play none none none',
+        once: true,
+    });
+});
+   
+ });
